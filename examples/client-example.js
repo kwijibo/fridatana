f6a504560dc03b9b8740493d2e9e7a248d30c44d
Keith Alexander <keithalexander@keithalexander.co.uk>
2016-10-11 10:39:18 +0100

    made the example a bit more interesting - though the updates apply too often unfortunately

--- examples/client-example.js
+++ examples/client-example.js
@@ -1,12 +1,39 @@
 //App
 import {fetchQuery, subscribeUpdates} from '../client.js'
 import {run} from './interpreter-example.js'
+import {lift} from 'ramda'
 
 //pure program 
-const fetchApp = fetchQuery({type: 'Person'}).map(results => `<ul>
-    ${results.map(({email, name}) =>
-        `<li><a href="mailto:${email}">${name}</a></li>`)}
-</ul><script>${JSON.stringify(results)}</script>`)
+const Friends = fetchQuery({type: 'Person'}).map(results => `
+<ul>
+    ${results.map(({email, name}) =>`
+        <li><a href="mailto:${email}">${name}</a></li>
+    `).join(' ')}
+</ul>`)
 
+const Posts = fetchQuery({type: 'Post'}).map(results => {
+   return `<div>
+    <textarea id="status-update">What's your status?</textarea>
+    ${results.map((result) => `
+        <div class="post">
+            <h4>${result.author.name}</h4>
+            <p>${result.content}</p>
+        </div>
+    `).join(' ')}
+    </div>
+`})
 
-run(fetchApp)
+const friendFeed = lift((friends, posts)=>`
+<div>
+       <h1>FriendFace</h1> 
+    <section id="friend-list">
+        <h2>Your Friends</h2>
+        ${friends}
+    </section>
+    <section id="activity-feed">
+        ${posts}
+    </section>
+</div>
+`)(Friends, Posts)
+
+run(friendFeed)
--- examples/interpreter-example.js
+++ examples/interpreter-example.js
@@ -6,31 +6,47 @@ import {dispatch} from 'freeky'
 const interpret = dispatch.dispatch
 
 // canned results for testing
-const results = {list: ['joe', 'simon'], index: {
+const people_results = {list: ['joe', 'simon'], index: {
     'joe': { name: 'Joe', email: 'joe@gmail.com', friend: 'donna'},
     'simon': { name: 'Simon', email: 'simon@hotmail.com'},
     'donna': { name: 'Donna', email: 'donna@yahoo.com'},
 }}
+const posts_results = {
+    list: [1,2,3],
+    index: {
+          1: { author: 'joe', content: `You won't believe what Bungle from Rainbow looks like now` }
+        , 2: { author: 'donna', content: `Say goodbye to herpes with this one weird old trick`}
+        , 3: { author: 'donna', content: `Most people won't share or like this but....`}
+        , 'joe': { name: 'Joe' }
+        , 'donna': { name: 'Donna' }
+    }
+}
+const results = {
+    Person: people_results,
+    Post: posts_results 
+}
 const update1 = {s: "joe", p: "name", o:"Joe Bloggs"}
 const update2 = {s: "joe", p: "email", o:"joe@example.info"}
 
 //transformers
-const fetchToTask = m => Task.of(results)
+const fetchToTask = m => Task.of(JSON.parse(JSON.stringify(results[m.query.type])))
 const updateSubscription = m => new Task((_,resolve)=>{ 
     resolve({})
     setTimeout(()=>resolve(update1), 1000) 
-    setTimeout(()=>resolve(update2), 2000) 
+    setTimeout(()=>resolve(update2), 3000) 
 })
 const updaterToTask = ({update, results}) => { 
    const {s,p,o} = update
    if(s) results.index[s][p] = o
    return Task.of(results)
 }
-const transformToTask = m => {
+const transformToTask = x => {
+    const m = Object.create(x)
     return Task.of(
         m.results.list.map(id => m.results.index[id])
                   .map(r => {
                     if(r.friend) r.friend = m.results.index[r.friend]
+                    if(r.author) r.author = m.results.index[r.author]
                     return r
                   })
     )
